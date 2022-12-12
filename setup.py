7fb81c9ab920863c2a762fd103660a07e7e4b11e
ravsmarty <65561109+ravsmarty@users.noreply.github.com>
2022-12-12 08:17:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="sensor",
     version="0.0.2",
     author="ineuron",
-    author_email="ravsarty121@gmail.com",
+    author_email="ravsmarty121@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
